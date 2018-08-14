9eae6d9e1dc4b041a223e840cfcf0aa064666328
Francisco Godinho <f.miguelgodinho@gmail.com>
2018-08-14 02:47:52 +0100

    synched read and writes

--- src/core/XSPSocketConnection.java
+++ src/core/XSPSocketConnection.java
@@ -8,61 +8,51 @@ import java.nio.ByteBuffer;
 import com.etsy.net.UnixDomainSocket;
 
 public class XSPSocketConnection {
-	
+
 	private String description;
 	private InputStream is;
 	private OutputStream os;
-	
-	
-	public XSPSocketConnection(String description, UnixDomainSocket unixDomainSocket)
-	throws IOException
-	{
+
+	public XSPSocketConnection(String description, UnixDomainSocket unixDomainSocket) throws IOException {
 		this.description = description;
 		is = unixDomainSocket.getInputStream();
 		os = unixDomainSocket.getOutputStream();
 	}
-	
-	public int receive(byte[] recv) 
-	throws IOException 
-	{
+
+	public synchronized int receive(byte[] recv) throws IOException {
 		int readBytes = is.read(recv);
-//		System.out.println(description + " received: " + new String(recv, "UTF-8"));
+		// System.out.println(description + " received: " + new String(recv, "UTF-8"));
 		return readBytes;
 	}
-	
-	public void send(CallType t, byte[] payload)
-	throws IOException
-	{
+
+	public void send(CallType t, byte[] payload) throws IOException {
 		if (t == null)
 			throw new IllegalArgumentException("Call cannot be null!");
-		
+
 		byte[] callNameBytes = t.getCallName().getBytes("UTF-8");
-		
-		ByteBuffer bb = payload != null ? 
-			ByteBuffer.allocate(callNameBytes.length + payload.length + 1) :
-			ByteBuffer.allocate(callNameBytes.length);
-			
+
+		ByteBuffer bb = payload != null ? ByteBuffer.allocate(callNameBytes.length + payload.length + 1)
+				: ByteBuffer.allocate(callNameBytes.length);
+
 		bb.put(callNameBytes);
-		
-		int delimiterpos = -1; 
-		
+
+		int delimiterpos = -1;
+
 		if (payload != null) {
 			delimiterpos = bb.position();
 			bb.put((byte) 0x0a);
 			bb.put(payload);
 		}
-		
-		
-		os.write(bb.array());
-		os.flush();
-//		System.out.println(description + " sent: " + new String(delimiterpos > 0 ? 
-//				bb.put(delimiterpos, (byte) 0x0a).array() : bb.array(), "UTF-8"));
+
+		synchronized (this) {
+			os.write(bb.array());
+			os.flush();
+		}
+		// System.out.println(description + " sent: " + new String(delimiterpos > 0 ?
+		// bb.put(delimiterpos, (byte) 0x0a).array() : bb.array(), "UTF-8"));
 	}
-	
-	
-	public void send(CallType t)
-	throws IOException
-	{
+
+	public void send(CallType t) throws IOException {
 		this.send(t, null);
 	}
 }
